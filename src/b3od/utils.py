d01ae62272d304c36017552f569398d117b90b58
Rafael Elter <rafael.elter@mbochip.com>
2023-11-24 16:38:22 -0300

    fix: any -> Any

--- src/b3od/utils.py
+++ src/b3od/utils.py
@@ -1,9 +1,10 @@
 """Usefull functions of the application"""
 
 from datetime import datetime, date
+from typing import Any
 
 
-def parse_date(date_to_parse: any) -> date:
+def parse_date(date_to_parse: Any) -> date:
     """Parses a date from a string or a datetime object"""
     if isinstance(date_to_parse, date):
         return date_to_parse
@@ -14,12 +15,12 @@ def parse_date(date_to_parse: any) -> date:
     raise ValueError(f"Invalid date format {date_to_parse}")
 
 
-def is_string(seq: any) -> bool:
+def is_string(seq: Any) -> bool:
     """Checks if a variable is a string"""
     return isinstance(seq, str)
 
 
-def is_sequence(seq: any) -> bool:
+def is_sequence(seq: Any) -> bool:
     """Checks if a variable is a sequence"""
 
     if is_string(seq):
